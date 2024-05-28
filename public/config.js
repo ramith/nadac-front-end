157c592ba905ef1c6fc89429c723a31f74c990d8
Ramith Jayasinghe <3402882+ramith@users.noreply.github.com>
2024-05-28 21:17:59 +0530

    remove config.js

--- public/config.js
+++
@@ -1,4 +0,0 @@
-// public/config.js
-window.configs = {
-    apiUrl: 'https://08bd1175-7029-4caf-a11b-10d047fed4f7-dev.e1-us-east-azure.choreoapis.dev/graphqldemo/nadacdataapi/nadac-graphql-api-ballerina-implementation--591/v1.0/',
-};
