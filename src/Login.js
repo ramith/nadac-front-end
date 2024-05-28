8f3e6484d9257eecd52deaa8e8b5065df27fe75c
Ramith Jayasinghe <3402882+ramith@users.noreply.github.com>
2024-05-28 18:58:48 +0530

    adding a login page

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate('/auth/login');
-  };
-
   return (
     <div className="Login">
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={() => { window.location.href = "/auth/login"; }}>Login</button>
     </div>
   );
 }
